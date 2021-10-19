d2bf9c3eb823c5aee3ce966fc54f5f1b757da176
bhupesh <bhupesh198817@gmail.com>
2021-10-19 20:02:10 +0100

    first commit

---
+++ src/PersonalDetails.java
@@ -0,0 +1,20 @@
+public class PersonalDetails {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Bhupesh");
+        System.out.println("22 Coloma Court GL51ba");
+        System.out.println("Salaries £28000");
+        System.out.println("12 percntage 60% ");
+        System.out.println("i like git 4");
+        System.out.println( "i like 4");
+
+
+    }
+
+
+
+
+
+}
